5001115983eaa2188a3baeac409c2cef6d9e3cd6
Lyuboslav Yotsov <114014625+LyuboslavYotsov@users.noreply.github.com>
2022-09-25 17:10:01 +0300

    Adding new features
    Score track and choice for new round

--- RockPaperScissors/RockPaperScissors/RockPaperScissors.cs
+++ RockPaperScissors/RockPaperScissors/RockPaperScissors.cs
@@ -10,62 +10,80 @@ namespace RockPaperScissors
             const string paper = "Paper";
             const string scissors = "Scissors";
 
-            Console.Write("Choose [r]ock, [p]aper or [s]cissors: ");
-            string playerMove = Console.ReadLine();
+            int playerScore = 0;
+            int computerScore = 0;
+            bool newGame = true;
 
-            if (playerMove == "r" || playerMove == "rock")
+            while (newGame)
             {
-                playerMove = rock;
-            }
-            else if (playerMove == "p" || playerMove == "paper")
-            {
-                playerMove = paper;
-            }
-            else if (playerMove == "s" || playerMove == "scissors")
-            {
-                playerMove = scissors;
-            }
-            else
-            {
-                Console.WriteLine("Invalid Input. Try Again ...");
-                return;
-            }
 
-            Random random = new Random();
-            int computerRandomNumber = random.Next(1, 4);
-            string computerMove = "";
-            switch (computerRandomNumber)
-            {
-                case 1:
-                    computerMove = rock;
-                    break;
-                case 2:
-                    computerMove = paper;
-                    break;
-                case 3:
-                    computerMove = scissors;
-                    break;
-            }
+                Console.Write("Choose [r]ock, [p]aper or [s]cissors: ");
+                string playerMove = Console.ReadLine();
 
-            Console.WriteLine($"The computer chose {computerMove}.");
-            if (playerMove == rock && computerMove == scissors ||
-                playerMove == paper && computerMove == rock ||
-                playerMove == scissors && computerMove == paper)
-            {
-                Console.WriteLine("You win.");
-            }
-            else if (playerMove == scissors && computerMove == rock ||
-               playerMove == rock && computerMove == paper ||
-               playerMove == paper && computerMove == scissors)
-            {
-                Console.WriteLine("You lose.");
-            }
-            else
-            {
-                Console.WriteLine("This game was a draw.");
-            }
+                if (playerMove == "r" || playerMove == "rock")
+                {
+                    playerMove = rock;
+                }
+                else if (playerMove == "p" || playerMove == "paper")
+                {
+                    playerMove = paper;
+                }
+                else if (playerMove == "s" || playerMove == "scissors")
+                {
+                    playerMove = scissors;
+                }
+                else
+                {
+                    Console.WriteLine("Invalid Input. Try Again ...");
+                    return;
+                }
 
+                Random random = new Random();
+                int computerRandomNumber = random.Next(1, 4);
+                string computerMove = "";
+                switch (computerRandomNumber)
+                {
+                    case 1:
+                        computerMove = rock;
+                        break;
+                    case 2:
+                        computerMove = paper;
+                        break;
+                    case 3:
+                        computerMove = scissors;
+                        break;
+                }
 
+                Console.WriteLine($"The computer chose {computerMove}.");
+                if (playerMove == rock && computerMove == scissors ||
+                    playerMove == paper && computerMove == rock ||
+                    playerMove == scissors && computerMove == paper)
+                {
+                    Console.WriteLine("You win.");
+                    playerScore++;
+                }
+                else if (playerMove == scissors && computerMove == rock ||
+                   playerMove == rock && computerMove == paper ||
+                   playerMove == paper && computerMove == scissors)
+                {
+                    Console.WriteLine("You lose.");
+                    computerScore++;
+                }
+                else
+                {
+                    Console.WriteLine("This game was a draw.");
+                }
+
+                Console.WriteLine("[n]ew game or [q]uit.");
+                string newGameCoice = Console.ReadLine();
+                if (newGameCoice == "q" || newGameCoice == "quit")
+                {
+                    newGame = false;
+                }
+            }
+            Console.WriteLine($"Final score is: Player -> {playerScore} | Computer -> {computerScore}");
+
+          
 
 
         }
